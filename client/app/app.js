4104a0088da5574e14879cf6fbb0f79c72e488f1
Yixiang Xu <yixiangxu@umass.edu>
2017-04-13 16:57:15 -0400

    failbook

--- client/app/app.js
+++ client/app/app.js
@@ -9,6 +9,7 @@ import FeedItem from './components/feeditem';
 import {hideElement} from './util';
 import {searchForFeedItems, deleteFeedItem} from './server';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router'
+import ErrorBanner from './components/errorbanner';
 
 /**
  * A fake profile page.
@@ -137,6 +138,11 @@ class App extends React.Component {
       <div>
         <NavBar searchTerm={searchTerm} />
         <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <ErrorBanner />
+            </div>
+          </div>
           <div className="row">
             <div className="col-md-2 fb-left-sidebar">
               <LeftSideBar />
